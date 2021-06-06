55255b32d9cf078bc78e97d40522c947ccff3842
pierreadorni <pierreadorni@free.fr>
2021-06-06 14:29:02 +0200

    working tab+stack navigator

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import HomeScreen from './components/home';
+import UploadScreen from './components/upload';
 
-export default function App() {
+
+const HomeStack = createStackNavigator();
+
+function HomeStackScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+    </HomeStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const UploadStack = createStackNavigator();
+
+function UploadStackScreen() {
+  return (
+    <UploadStack.Navigator>
+        <UploadStack.Screen name="Upload" component={UploadScreen} />
+    </UploadStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === 'Home') {
+                iconName = focused
+                  ? 'ios-information-circle'
+                  : 'ios-information-circle-outline';
+              } else if (route.name === 'Upload') {
+                iconName = focused ? 'cloud-upload' : 'cloud-upload-outline';
+              }
+
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}
+        >
+          <Tab.Screen name="Home" component={HomeStackScreen} />
+          <Tab.Screen name="Upload" component={UploadStackScreen} />
+        </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ components/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {StyleSheet, View, Text, ActivityIndicator, ScrollView, Image} from "react-native";
+
+class Home extends React.Component{
+    constructor(props){
+            super(props);
+    }
+    render(){
+        return (
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home!</Text>
+          </View>
+        );
+    }
+}
+
+export default Home;
