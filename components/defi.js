7f67d34755c73a2ca6e48c736d10e775aef4df5d
pierreadorni <pierreadorni@free.fr>
2021-06-21 11:42:26 +0200

    modal de choix de défi qui fonctionne, reste les props à ajouter

---
+++ components/defi.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+class Defi extends React.Component {
+    render(){
+        return (
+            <Text> OUI BONJOUR </Text>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Defi;
