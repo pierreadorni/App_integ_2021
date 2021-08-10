5a5436f55a3cada2816f1181c5e565ad732a56cc
pierreadorni <pierreadorni@free.fr>
2021-08-10 14:11:31 +0200

    front wei

--- screens/home.js
+++ screens/home.js
@@ -18,7 +18,9 @@ import {
 } from "react-native-responsive-screen";
 import { LinearGradient } from "expo-linear-gradient";
 import MapView from "react-native-maps";
-import { Marker } from "react-native-maps";
+import { Marker, Callout } from "react-native-maps";
+import * as Linking from 'expo-linking';
+import { Table, TableWrapper, Row, Rows, Col } from 'react-native-table-component';
 
 class Home extends React.Component {
   state = {
@@ -26,11 +28,34 @@ class Home extends React.Component {
     events: [],
     zoom: -1,
     isLoading: true,
+    TUCMarkers: [],
+    weiInfos: {
+      "billetterie":"https://assos.utc.fr/integ",
+      "bieres-aprem":{
+        "Mystic Blanche Cerise": 2.5,
+        "Cidre Pas Pareil": 2.4,
+        "Limonade": 1
+      },
+      "bieres-soir":{
+        "Rebelle Blonde": 2.6,
+        "Kasteel Rouge": 2.95,
+        "Cuvée des Trolls: ":2.8,
+        "Cidre Pas Pareil": 2.4,
+        "Limonade": 1
+      },
+      "Lineup":[
+        ["Grand Soleil","Pastel coast","Figurz"],
+        ["FSC","SCPB","Batello","Norann","Léo Mercier"]
+      ]
+    }
   };
   specialZooms = {
     tuc: () => {
       return this._displayTUC();
     },
+    wei: () => {
+      return this._displayWEI();
+    }
   };
 
   async _getPlanning() {
@@ -46,6 +71,50 @@ class Home extends React.Component {
     this.setState({
       events: res,
     });
+  }
+
+  async _getWeiInfos(){
+    return 1;
+  }
+
+
+
+  _createTUCMarker(remainingMarkers){
+    const marker = remainingMarkers.pop();
+    console.log(marker.latitude, marker.longitude)
+    if (remainingMarkers.length == 0){
+      console.log('last marker');
+      
+      return (
+        <Marker coordinate={{ latitude: parseFloat(marker.latitude), longitude: parseFloat(marker.longitude) }}>
+          <Callout style={{maxWidth:150, alignItems:'center'}}>
+            <Text style={{fontSize:16, textAlign:'center', fontWeight:'bold'}}>{marker.nom}</Text>
+            <Text style={{fontSize:12, textAlign:'center', color:'blue', fontStyle:'italic'}}>{marker.adresse}</Text>
+            <Text style={{fontSize:12, textAlign:'justify', width:100}}>{marker.description}</Text>
+          </Callout>
+        </Marker>
+      )
+    }else{
+      return (
+        <View>
+          <Marker coordinate={{ latitude: parseFloat(marker.latitude), longitude: parseFloat(marker.longitude) }}>
+          <Callout style={{maxWidth:150, alignItems:'center'}}>
+            <Text style={{fontSize:16, textAlign:'center', fontWeight:'bold'}}>{marker.nom}</Text>
+            <Text style={{fontSize:12, textAlign:'center', color:'blue', fontStyle:'italic'}}>{marker.adresse}</Text>
+            <Text style={{fontSize:12, textAlign:'justify', width:100}}>{marker.description}</Text>
+          </Callout>
+        </Marker>
+          {this._createTUCMarker(remainingMarkers)}
+        </View>
+      )
+    }
+  }
+  async _getTUCMarkers(){
+    let data = await fetch("https://assos.utc.fr/integ/integ2021/api/get-tuc-markers.php");
+    data = await data.json();
+    this.setState({
+      TUCMarkers: data
+    })
   }
 
   _displayTUC() {
@@ -103,8 +172,10 @@ class Home extends React.Component {
               {this.state.events[this.state.zoom].description}
             </Text>
 
-            <Text style={styles.about}>Autre</Text>
+            <Text style={styles.about}>Carte des chantiers</Text>
             <MapView
+
+              userInterfaceStyle="dark"
               initialRegion={{
                 latitude: this.state.events[this.state.zoom].latitude,
                 longitude: this.state.events[this.state.zoom].longitude,
@@ -113,19 +184,147 @@ class Home extends React.Component {
               }}
               style={styles.map}
             >
-              <Marker
-                coordinate={{
-                  latitude: this.state.events[this.state.zoom].latitude,
-                  longitude: this.state.events[this.state.zoom].longitude,
-                }}
-                title={this.state.events[this.state.zoom].title}
-              />
+            {this._createTUCMarker(this.state.TUCMarkers)}
             </MapView>
           </View>
           <View style={{ paddingBottom: 100 }}></View>
         </View>
       </ScrollView>
     );
+  }
+
+  _dictToArray(dict){
+    let a = []
+    for (let [key, value] of Object.entries(dict)) {
+      a.push([key,value+'€']);
+    }
+    return a
+  }
+
+  _displayWEI(){
+    console.log(Object.values(this.state.weiInfos['bieres-aprem']))
+    return(
+      <ScrollView
+            style={styles.zoomed}
+            showsVerticalScrollIndicator={false}
+          >
+            <View style={styles.zoomedEvent}>
+              <ImageBackground
+                style={styles.zoomedImg}
+                source={{uri:this.state.events[this.state.zoom].img}}
+              >
+                <LinearGradient
+                  colors={["rgba(18,18,18,0)", "rgba(18,18,18,0.76)"]}
+                  style={styles.overlay}
+                >
+                  <View style={styles.zoomedBottom}>
+                    <TouchableOpacity
+                      style={styles.leave}
+                      onPress={() => this.setState({ zoom: -1 })}
+                    >
+                      <Image
+                        source={require("../assets/cross.png")}
+                        style={styles.cross}
+                      />
+                    </TouchableOpacity>
+                    <View style={styles.dateBtn}>
+                      <Text style={styles.day}>
+                        {this.state.events[this.state.zoom].day}
+                      </Text>
+                      <Text style={styles.month}>
+                        {this.state.events[this.state.zoom].month}
+                      </Text>
+                    </View>
+                  </View>
+                </LinearGradient>
+              </ImageBackground>
+              <View style={styles.zoomedMain}>
+                <View style={styles.location}>
+                  <Image
+                    source={require("../assets/location.png")}
+                    style={styles.locationIcon}
+                  />
+                  <Text style={styles.locationText}>
+                    {this.state.events[this.state.zoom].location}
+                  </Text>
+                </View>
+                <Text style={styles.title}>
+                  {this.state.events[this.state.zoom].title}
+                </Text>
+                <Text style={styles.hours}>
+                  {this.state.events[this.state.zoom].hours}
+                </Text>
+
+                <Text style={styles.about}>Billetterie</Text>
+                
+                <TouchableOpacity onPress={()=>{Linking.openURL(this.state.weiInfos.billetterie);}}>
+                  <Text style={styles.link}>
+                    {this.state.weiInfos.billetterie}
+                  </Text>
+                </TouchableOpacity>
+                  
+                <Text style={styles.about}>Lineup</Text>
+                <View style={{width:'100%',justifyContent:'center'}}>
+                  <Text adjustsFontSizeToFit numberOfLines={1} style={{color:'white',fontSize:24, textAlign:'center', marginTop:20}}>
+                    {
+                      this.state.weiInfos.Lineup[0].join(' • ')
+                    }
+                  </Text>
+                  <Text adjustsFontSizeToFit numberOfLines={1} style={{color:'white',fontSize:16, textAlign:'center', marginTop:5}}>
+                    {
+                      this.state.weiInfos.Lineup[1].join(' • ')
+                    }
+                  </Text>
+                </View>
+
+                <Text style={styles.about}>Bar</Text>
+
+                <View style={{width:'100%',justifyContent:'center'}}>
+                  <Text style={{color:'white', textDecorationLine:'underline', textAlign:'center'}}>Après Midi</Text>
+
+                  <Table borderStyle={{ borderWidth: 0, borderColor:"#EA8BDE" }} style={{marginTop:20}}>
+                    <Row
+                      data={["Boisson","Prix"]}
+                      flexArr={[2, 1]}
+                      style={{}}
+                      textStyle={{color:'white', textAlign:'center', fontWeight:'bold', fontSize:16}}
+                    />
+                    <TableWrapper style={{}}>
+                      <Rows
+                        data={this._dictToArray(this.state.weiInfos['bieres-aprem'])}
+                        flexArr={[2, 1]}
+                        style={{}}
+                        textStyle={{color:'white', textAlign:'center', lineHeight:20}}
+                      />
+                    </TableWrapper>
+                  </Table>
+
+                  <Text style={{color:'white', textDecorationLine:'underline', textAlign:'center'}}>Soir</Text>
+
+                  <Table borderStyle={{ borderWidth: 0, borderColor:"#EA8BDE" }} style={{marginTop:20}}>
+                    <Row
+                      data={["Boisson","Prix"]}
+                      flexArr={[2, 1]}
+                      style={{}}
+                      textStyle={{color:'white', textAlign:'center', fontWeight:'bold', fontSize:16}}
+                    />
+                    <TableWrapper style={{}}>
+                      <Rows
+                        data={this._dictToArray(this.state.weiInfos['bieres-soir'])}
+                        flexArr={[2, 1]}
+                        style={{}}
+                        textStyle={{color:'white', textAlign:'center', lineHeight:20}}
+                      />
+                    </TableWrapper>
+                  </Table>
+
+                </View>
+
+              </View>
+              <View style={{ paddingBottom: 100 }}></View>
+            </View>
+          </ScrollView>
+    )
   }
 
   _getEventById(id) {
@@ -201,6 +400,9 @@ class Home extends React.Component {
 
                 <Text style={styles.about}>Autre</Text>
                 <MapView
+
+                  userInterfaceStyle="dark"
+
                   initialRegion={{
                     latitude: this.state.events[this.state.zoom].latitude,
                     longitude: this.state.events[this.state.zoom].longitude,
@@ -230,6 +432,7 @@ class Home extends React.Component {
     this._getPlanning().then(() => {
       this.setState({ isLoading: false });
     });
+    this._getTUCMarkers();
   }
 
   _renderFlatList() {
@@ -493,6 +696,15 @@ const styles = StyleSheet.create({
     lineHeight: 18,
     color: "#FFFFFF",
     opacity: 0.82,
+    marginTop: hp("2%"),
+  },
+  link:{
+    fontSize: 13,
+    letterSpacing: 0.24,
+    lineHeight: 18,
+    color: "#00bbe1",
+    opacity: 0.82,
+    textDecorationLine:"underline",
     marginTop: hp("2%"),
   },
   leave: {
